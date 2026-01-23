56c92bd42af204924c00058dadc0aafdcf2cc095
istiakAHMEDsaad <istik2020@gmail.com>
2026-01-23 16:04:05 +0600

    fixed style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,33 +12,37 @@ const PoppinsFont = Poppins({
 
 export default function Home() {
   return (
-    <div className='h-full'>
-      <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
-        {/* IMAGE CONTAINER */}
-        <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
-          <Image src='/hero-image.png' alt='' fill className='object-contain' />
+    <div className='h-full flex flex-col lg:flex-row px-4 ms:px-8 md:px-12 lg:px-20 xl:px-48'>
+      <div className='flex flex-col md:flex-col lg:flex-row'>
+        {/* image container */}
+        <div className='lg:flex-1 h-full w-full'>
+          <div className='h-80 lg:h-110 w-full relative mx-auto'>
+            <Image
+              src='/hero-image.png'
+              fill
+              alt=''
+              className='object-contain'
+            />
+          </div>
         </div>
-        {/* TEXT CONTAINER */}
-        <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
-          {/* TITLE */}
-          <h1 className='text-4xl md:text-6xl font-bold'>
-            Crafting Digital Experiences, Designing Tomorrow.
-          </h1>
-          {/* DESC */}
-          <p className='md:text-xl'>
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
-          </p>
-          {/* BUTTONS */}
-          <div className='w-full flex gap-4'>
-            <button className='p-4 rounded-lg ring-1 ring-black bg-black text-white'>
-              View My Work
-            </button>
-            <button className='p-4 rounded-lg ring-1 ring-black'>
-              Contact Me
-            </button>
+
+        {/* text container */}
+        <div className='lg:flex-1 mt-6 lg:mt-0 lg:pt-8'>
+          <div className='flex flex-col items-center justify-center w-full gap-6'>
+            {/* title */}
+            <h1
+              className={`text-4xl md:text-5xl lg:text-6xl font-bold ${InterFont.className}`}
+            >
+              Crafting Digital Experiences, Designing Tomorrow.
+            </h1>
+
+            {/* description */}
+            <p className={`md:text-xl ${PoppinsFont.className}`}>
+              Welcome to my digital canvas, where innovation and creativity
+              converge. With a keen eye for aesthetics and a mastery of code, my
+              portfolio showcases a diverse collection of projects that reflect
+              my commitment to excellence.
+            </p>
           </div>
         </div>
       </div>
